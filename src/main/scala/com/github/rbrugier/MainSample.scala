e320a0dca2e84b5cfbc50e9725d781fc252deca0
Raphael Brugier <raphael.brugier@gmail.com>
2016-02-25 16:01:19 -0500

    draft test

---
+++ src/main/scala/com/github/rbrugier/MainSample.scala
@@ -0,0 +1,27 @@
+package com.github.rbrugier
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+
+object MainSample {
+
+  //val sparkConf = new SparkConf()
+  //  .setMaster("local[*]")
+   // .setAppName("spark-test-example")
+  //val sc = new SparkContext(sparkConf)
+
+  def main(args: Array[String]) {
+    println("Hello")
+    //val rdd = sc.textFile("src/main/resources/learningSparkIntroduction.txt")
+    //count(rdd).foreach(println)
+  }
+
+  def count(lines: RDD[String]): RDD[(String, Int)] = {
+    val wordsCount = lines.flatMap(l => l.split("\\W+"))
+        .map(word => word.trim())
+      .map(word => (word, 1))
+      .reduceByKey(_ + _)
+    wordsCount
+  }
+}
---
+++ src/test/scala/MainSampleTest.scala
@@ -0,0 +1,36 @@
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.rdd.RDD
+import org.scalatest.{BeforeAndAfter, Matchers, FlatSpec}
+import com.github.rbrugier.MainSample
+
+class MainSampleTest extends FlatSpec with Matchers with BeforeAndAfter {
+
+  var sc:SparkContext = _
+
+  before {
+    val sparkConf = new SparkConf()
+      .setMaster("local[*]")
+      .setAppName("test-rdd")
+    sc = new SparkContext(sparkConf)
+  }
+
+  after {
+    sc.stop()
+  }
+
+  behavior of "Count words"
+
+  it should "count words in lines" in {
+    val text =
+      """Hello world
+        |Hello
+      """.stripMargin
+    val lines: RDD[String] = sc.parallelize(List(text))
+    val wordCounts: RDD[(String, Int)] = MainSample.count(lines)
+
+    wordCounts.foreach(it => println(it))
+
+    wordCounts.collect() should contain allOf (("Hello", 2), ("world", 1))
+  }
+
+}
