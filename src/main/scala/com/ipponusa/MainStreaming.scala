6ec1bb2d36eb5f8d9a7381a6e9a44bae995b85b0
Raphael Brugier <raphael.brugier@gmail.com>
2016-03-07 12:55:46 -0500

    rewrite MainStreaming with windowing and no delegation to StreamOperations

--- src/main/scala/com/ipponusa/MainStreaming.scala
+++ src/main/scala/com/ipponusa/MainStreaming.scala
@@ -2,38 +2,32 @@ package com.ipponusa
 
 import org.apache.spark.SparkConf
 import org.apache.spark.rdd.RDD
-import org.apache.spark.streaming.dstream.{DStream, InputDStream}
 import org.apache.spark.streaming.{Seconds, StreamingContext}
-
 import scala.collection.mutable
 
 object MainStreaming {
 
   val sparkConf = new SparkConf()
     .setMaster("local[*]")
-    .setAppName("spark-testing-example")
+    .setAppName("spark-streaming-testing-example")
 
-  val ssc = new StreamingContext(sparkConf, Seconds(5))
+  val ssc = new StreamingContext(sparkConf, Seconds(1))
 
   def main(args: Array[String]) {
 
     val rddQueue = new mutable.Queue[RDD[Char]]()
 
-    val input: InputDStream[Char] = ssc.queueStream(rddQueue, oneAtATime = false)
-    StreamOperations.capitalize(input)
+    ssc.queueStream(rddQueue)
+      .map(_.toUpper)
+      .window(windowDuration = Seconds(3), slideDuration = Seconds(2))
       .print()
 
     ssc.start()
 
     for (c <- 'a' to 'z') {
-      println("c = " + c)
       rddQueue += ssc.sparkContext.parallelize(List(c))
-      Thread.sleep(Seconds(1).milliseconds)
     }
 
-    println("await termination")
     ssc.awaitTermination()
   }
-
-
 }
